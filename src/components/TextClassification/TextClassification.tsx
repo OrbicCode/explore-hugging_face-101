c4d1b9baa179dfc0929f8fa8fab16570cb7b36b5
James O'Kane <orbiccode@gmail.com>
2025-12-02 13:03:50 +0000

    handleclick function handled

--- src/components/TextClassification/TextClassification.tsx
+++ src/components/TextClassification/TextClassification.tsx
@@ -1,12 +1,45 @@
+import { InferenceClient } from '@huggingface/inference';
 import Button from '../Button/Button';
 import styles from './TextClassification.module.css';
+import { useState } from 'react';
+
+interface Classification {
+  label: string;
+  score: number;
+}
+
+const hf = new InferenceClient(import.meta.env.VITE_HF_TOKEN);
 
 export default function TextClassification() {
-  function handleClick() {}
+  const [classification, setClassification] = useState<Classification[] | null>(null);
+
+  async function handleClick() {
+    try {
+      const response = await hf.textClassification({
+        inputs: 'hello, I like you',
+        provider: 'hf-inference',
+      });
+      setClassification(response as Classification[]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const display =
+    classification &&
+    classification.map((clas: Classification) => {
+      return (
+        <li key={clas.label}>
+          <p>{clas.label}</p>
+          <p>{clas.score}</p>
+        </li>
+      );
+    });
   return (
     <div className={styles.container}>
       <h2>Text Classification</h2>
       <Button onClick={handleClick} text='Text Classification' />
+      <ul>{display}</ul>
     </div>
   );
 }
