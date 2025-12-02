69fbf39b6b710188183a42145820ec06ac5a762f
James O'Kane <orbiccode@gmail.com>
2025-12-02 11:42:50 +0000

    button styling and huggingface inference install

--- src/components/TextGeneration/TextGeneration.tsx
+++ src/components/TextGeneration/TextGeneration.tsx
@@ -6,7 +6,9 @@ export default function TextGeneration() {
   const [generatedText, setGeneratedText] = useState('');
 
   function handleClick() {
-    setGeneratedText('LOTR is the best');
+    setGeneratedText(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id sem sodales, vulputate augue eget, dapibus elit. Mauris tortor elit, blandit et fermentum eget, pharetra sed lectus. Integer ac semper erat. Nunc molestie nisi dui, sed dignissim dui pellentesque eu. Duis rutrum purus id quam fringilla tincidunt. Morbi tempor nunc a luctus porttitor. Etiam ac efficitur tortor. Cras volutpat mauris et laoreet venenatis. Suspendisse accumsan eget lorem a suscipit. Suspendisse aliquam dictum nulla sit amet ultrices. Donec vulputate porta semper. Aliquam a mauris tellus. Donec finibus porta iaculis. Ut orci metus, euismod nec consectetur dictum, lacinia eget odio. Vestibulum vehicula tortor in nisi varius aliquet. Mauris vel tincidunt tellus.'
+    );
   }
 
   return (
