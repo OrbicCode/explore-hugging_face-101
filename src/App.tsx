e0eac58d80fc24f27b713ce9545c926f597313c8
James O'Kane <orbiccode@gmail.com>
2025-12-02 12:44:13 +0000

    TextClassification Shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import TextClassification from './components/TextClassification/TextClassification';
 import TextGeneration from './components/TextGeneration/TextGeneration';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <h1>Explore Hugging Face 101</h1>
       <div className='widget_container'>
         <TextGeneration />
+        <TextClassification />
       </div>
     </main>
   );
---
+++ src/components/TextClassification/TextClassification.tsx
@@ -0,0 +1,9 @@
+import styles from './TextClassification.module.css';
+
+export default function TextClassification() {
+  return (
+    <div className={styles.container}>
+      <h2>TextClassification</h2>
+    </div>
+  );
+}
