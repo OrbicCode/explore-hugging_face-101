95bb198a49e781f2b29578c2e2ba00a48e10000f
James O'Kane <orbiccode@gmail.com>
2025-12-02 12:46:50 +0000

    button added

--- src/components/TextClassification/TextClassification.tsx
+++ src/components/TextClassification/TextClassification.tsx
@@ -1,9 +1,12 @@
+import Button from '../Button/Button';
 import styles from './TextClassification.module.css';
 
 export default function TextClassification() {
+  function handleClick() {}
   return (
     <div className={styles.container}>
-      <h2>TextClassification</h2>
+      <h2>Text Classification</h2>
+      <Button onClick={handleClick} text='Text Classification' />
     </div>
   );
 }
