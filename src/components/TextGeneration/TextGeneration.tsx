711262d2c8428614fd7566cf274f10fe10194c54
James O'Kane <orbiccode@gmail.com>
2025-12-02 11:08:22 +0000

    added button shell

--- src/components/TextGeneration/TextGeneration.tsx
+++ src/components/TextGeneration/TextGeneration.tsx
@@ -1,8 +1,11 @@
 export default function TextGeneration() {
+  function handleClick() {}
+
   return (
     <>
       <h2>Text Generation</h2>
       <p>Fonty Wonty</p>
+      <button onClick={handleClick}></button>
     </>
   );
 }
